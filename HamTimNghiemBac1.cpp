6ef0a71b17c76a9052a3666d9c43b97f4ce6f95f
Viết Dũng <65429963+vietdung1482000@users.noreply.github.com>
2020-05-20 13:30:43 +0700

    Hàm con tìm nghiệm bậc 1

---
+++ HamTimNghiemBac1.cpp
@@ -0,0 +1,13 @@
+#include"stdio.h"
+void bac1(float a,float b){
+	if (a==0)
+		if (b==0) printf("\n PT vo so nghiem.");
+		else printf("\n PT vo nghiem");
+	else
+		printf("\n Nghiem PT: %0.2f",-b/a);
+}
+int main(){
+	
+}
+	
+
