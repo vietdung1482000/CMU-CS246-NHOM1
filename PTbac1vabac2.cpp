d9617f0682d21ab998a4ae72c58644adac3508a4
hungnehk123 <65438793+hungnehk123@users.noreply.github.com>
2020-05-20 14:39:32 +0700

    Update PTbac1vabac2.cpp
    Đưa hàm bậc hai vào Main đơn giản thuật toán

--- PTbac1vabac2.cpp
+++ PTbac1vabac2.cpp
@@ -9,9 +9,10 @@ void bac1(float a,float b){
 		printf ("\n Nghiem PT : %0.2f",-b/a);
 }
 
-void bac2(float &a,float &b,float &c)
+int main()
 {
-	float dt;
+	float dt,a,b,c;
+	printf("\nNhap a,b,c: ");scanf("%f%f%f, &a,&b,&c);
 	if(a==0)
 		bac1(b,c);
 	else{	
@@ -20,31 +21,8 @@ void bac2(float &a,float &b,float &c)
 			printf("\n PT vo nghiem"); 
 		if (dt==0)
 			printf("\n PT co nghiem kep x = %0.2f",-b/2/a);	
-	    if (dt>0)
+	    	if (dt>0)
 			printf("\n PT co 2 nghiem: x1 = %.2f; x2 = %.2f",(-b+sqrt(dt))/2/a,(-b-sqrt(dt))/2/a);	
 	}
-}
-
-int main()
-{
-	float n,a,b,c;
-	printf("Moi ban chon 1 hoac 2 de chon loai pt: ");//chon pt bac 1 hoac pt bac 2
-	scanf("%f",&n);
-	if(n==1)
-		{
-			printf("Nhap a: ");scanf("%f",&a);
-			printf("Nhap b: ");scanf("%f",&b);
-			bac1(a,b);
-		}
-	else
-		if(n==2)
-			{	
-				printf("Nhap a: ");scanf("%f",&a);
-				printf("Nhap b: ");scanf("%f",&b);
-				printf("Nhap c: ");scanf("%f",&c);
-				bac2(a,b,c);
-			}
-		else	
-			printf("Error");//khong phai 1 va 2
-	return 0;
+	getch();
 }
